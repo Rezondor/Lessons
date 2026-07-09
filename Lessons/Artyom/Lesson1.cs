960cda77a021c4f83568301524163b784cec7d1b
SNill1 <art.bugrim@gmail.com>
2026-07-09 20:56:56 +0300

    Test 1

---
+++ Lessons/Artyom/Lesson1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lessons.Artyom
+{
+    internal class Lesson1
+    {
+        public static void foo()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
