1dc46ae075577e2c6b15c22f45a11f45430ed9c3
Snill <art.bugrim@gmail.com>
2026-07-09 20:43:41 +0300

    Test lesson 1

---
+++ Lessons/Artem/Lesson1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lessons.Artem
+{
+    internal class Lesson1
+    {
+        public static void func1(string inp)
+        {
+            bool isAlphaNumeric = false;
+            for (int i = 0; i < inp.Length; i++)
+            {
+                if (isAlphaNumeric) 
+                { 
+                }
+            }
+        }
+    }
+}
