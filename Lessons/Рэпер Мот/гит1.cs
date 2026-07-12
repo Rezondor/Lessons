c00977229f42e03831388002e96483ce553dee9e
SNill1 <art.bugrim@gmail.com>
2026-07-12 17:35:00 +0300

    Artem Lesson 3

--- Lessons/Рэпер Мот/гит1.cs
+++ Lessons/Рэпер Мот/гит1.cs
@@ -2,7 +2,7 @@
 
 internal class Program
 {
-    static void Main(string[] args)
+    static void main(string[] args)
     {
         Console.WriteLine("я зяблик");
         Console.WriteLine("я зяблик");
